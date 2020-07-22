dfbd14ed99d27456dd36d775051d4c6d53c8cfc2
zou_yp <zouyuanpin95@foxmail.com>
2020-07-22 21:06:08 +0800

    todolist页面制作（1）

---
+++ src/components/ToDoList/ToDoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="toDoList">
+      <div class="haveToDo">
+          <h2>还要做什么</h2>
+          <ol>
+              <li v-for="(item,i) in haveToDo" :key="i" @click="doItem">{{item}}</li>
+          </ol>
+      </div>
+      <div class="haveDone">
+          <h2>做完了什么</h2>
+           <ol>
+              <li v-for="(item,i) in haveDone" :key="i" @click="redoItem">{{item}}</li>
+          </ol>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+  data () {
+    return {
+      haveToDo: ['111', '22'],
+      haveDone: ['11221', '2332']
+    }
+  },
+  methods: {
+    doItem (item) {
+      console.log(item)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 
+// 全局引入ElementUI
+Vue.use(ElementUI)
+// 阻止vue 在启动时生成生产提示
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  render: h => h(App),
   components: { App },
   template: '<App/>'
 })
